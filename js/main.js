d3930d7fa75015fc89cffa847f4c41e816d7ce77
100june <toypiano@naver.com>
2023-12-25 09:21:44 +0900

    visual complete

--- js/main.js
+++ js/main.js
@@ -24,13 +24,23 @@ window.addEventListener('scroll', _.throttle(function() {
     // gsap.to(요소, 지속시간, 옵션);
     gsap.to(badgeEl, .6, {
       opacity: 0,
+      display: 'none'
     });
   } else {
     // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
+      display: 'block'
     });
   }
 }, 300));
-
 // _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl, index) {
+  // gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1,
+  });
+});
